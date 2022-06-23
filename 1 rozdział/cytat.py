91113833fb86a15111bd2db8bd6dd8b4b31879a2
BartekWilman <92676388+BartekWilman@users.noreply.github.com>
2022-06-23 11:34:53 +0200

    Add files via upload

---
+++ 1 rozdział/cytat.py
@@ -0,0 +1,18 @@
+# ulubiony cytat
+# zastosowanie funkcji print i funkcji input
+
+# treść cytatu
+
+print('"VENI, VIDI, VICI"')
+
+
+# autor cytatu
+
+print("Gaius Iulius Caesar")
+
+
+# oczekiwanie na zamknięcie programu
+
+input("\n\nAby zakończyć program naciśnij Enter")
+
+
---
+++ 1 rozdział/imię i nazwisko.py
@@ -0,0 +1,12 @@
+#imię i nazwisko
+#zastosowanie funkcji input
+
+
+#wyświetlenie imienia i nazwiska
+
+print("Nazywam się Bartłomiej Wilman")
+
+
+#oczekiwanie na zamknięcie programu
+
+input("\n\nAby zakończyć wciśniej klawisz Enter")
