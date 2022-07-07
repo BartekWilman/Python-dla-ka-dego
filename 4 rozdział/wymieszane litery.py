6eae6eee515da9a47408e5c0dc7c405cf3598942
BartekWilman <92676388+BartekWilman@users.noreply.github.com>
2022-07-07 16:10:56 +0200

    Add files via upload

--- 4 rozdział/wymieszane litery.py
+++ 4 rozdział/wymieszane litery.py
@@ -13,6 +13,7 @@ correct = word
 jumble = ""
 score = 0
 hint = 0
+guess = None
 
 #pętla mieszająca litery w wylosowanym słowie
 
@@ -32,40 +33,29 @@ print("Jako że jestem fanem Warhammera fantasy, kategorią są bogowie z tego u
 #początek gry
 print("\n\n\n\nZgadnij, co to za bóg:", jumble.upper())
 
-guess = input("Twoja odpowiedź to: ")
 
-if guess == correct:
-    score = len(correct) * 100 - (hint * 10)
-    print("Zgadza się")
-    print("Poprawna odpowiedź to", correct, ", a w trakcie gry uzyskałeś", score, "punktów\a")
+while guess != correct and guess != "":
 
-elif guess != correct and guess != "":
+    guess = input("Twoja odpowiedź to: ")
 
-    while guess != correct and guess != "":
+    if guess == correct:
+        score = len(correct) * 100 - (hint * 10)
+        print("Zgadza się")
+        print("Poprawna odpowiedź to", correct, ", a w trakcie gry uzyskałeś", score, "punktów\a")
 
-        if guess != correct:
+    elif guess != correct and guess != "":
 
-            print("Niestety nie...")
+        print("Niestety nie...")
+        
+        #pytanie o podpowiedź
+        ask = input("Chcesz podpowiedź? (T/N)... ")
 
-            #pytanie o podpowiedź
-            ask = input("Chcesz podpowiedź? (T/N)... ")
-            
-            if ask == "T":
-                print("Imię tego boga zaczyna się na", correct[0], ",a kończy na", correct[-1])
-                hint += len(correct) / 3
+        if ask == "T":
+            print("Imię tego boga zaczyna się na", correct[0], ",a kończy na", correct[-1])
+            hint += len(correct) / 3
+        else:
+            print("Słusznie, szkoda punktów ;)")
 
-            elif ask == "N":
-                print("Słusznie, szkoda punktów ;)")
-
-            guess = input("Twoja odpowiedź to: ")
-            score = len(correct) * 100 - (hint * 10)
-            print("Zgadza się")
-            print("Poprawna odpowiedź to", correct, ", a w trakcie gry uzyskałeś", score, "punktów\a")
-
-        elif guess == correct:
-            score = len(correct) * 100 - (hint * 10)
-            print("Zgadza się")
-            print("Poprawna odpowiedź to", correct, ", a w trakcie gry uzyskałeś", score, "punktów\a")
 
 print("Dzięki za udział w grze, mam nadzieję, że bawiłeś się tak samo dobrze jak ja pisząc ten program ;)")
 input()
